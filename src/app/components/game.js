12dff60d911c2170ea3de2b272a1631b6ee4ab04
WesleyFilhoDev <wesleyfilho221204@gmail.com>
2025-05-27 13:57:32 -0300

    constantes criadas

---
+++ src/app/components/game.js
@@ -0,0 +1,9 @@
+"use client";
+import { useEffect, useState } from "react";
+
+export function Game() {
+  const [numeroSecreto, setNumeroSecreto] = useState("");
+  const [tentativas, setTentativas] = useState(0);
+  const [input, setInput] = useState("");
+  const [jogoEncerrado, setJogoEncerrado] = useState(false);
+}
